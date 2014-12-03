6b56a81e45b4705a75437a6bbb750fe9edc36fd8
ibrahimacoundoul <Ibrahima.COUNDOUL@cpexterne.org>
2014-12-03 18:18:00 +0100

    commit auto des fichiers json modifiés bis

--- kiss-swagger-check-change.sh
+++ kiss-swagger-check-change.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+java -cp kissSwaggerDiff-0.0.1-SNAPSHOT.jar;/d/applis/MAVEN/m2repos/org/json/json/20140107/json-20140107.jar;/d/applis/GIT/kissSwaggerDiff/target/classes fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissSwaggerMainHandler
 cd /d/applis/GIT/kissSwaggerDiff
 git commit -am "commit auto des fichiers json modifiés bis"
 git push
-# java -cp kissSwaggerDiff-0.0.1-SNAPSHOT.jar;D:\applis\MAVEN\m2repos\org\json\json\20140107\json-20140107.jar;D:\applis\GIT\kissSwaggerDiff\target\classes fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissSwaggerMainHandler
