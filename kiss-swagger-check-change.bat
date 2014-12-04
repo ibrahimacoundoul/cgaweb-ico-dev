e6d3024e8264d877317503b6c9bf04c20b51c864
ibrahimacoundoul <Ibrahima.COUNDOUL@cpexterne.org>
2014-12-04 16:26:48 +0100

    commit auto des modifications des webservices KISS avec Swagger

--- kiss-swagger-check-change.bat
+++ kiss-swagger-check-change.bat
@@ -1,5 +1,5 @@
 java -cp kissSwaggerDiff-0.0.1-SNAPSHOT.jar;D:\applis\MAVEN\m2repos\org\json\json\20140107\json-20140107.jar;D:\applis\GIT\kissSwaggerDiff\target\classes fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissSwaggerMainHandler
-REM git.exe commit -am "test comit auto"
+REM on ajoute tous les nouveaux fichiers puis commiter et pusher sur la repos github 
 git add *
-git commit -am "commit auto des fichiers json modifiés 4 déc bis"
+git commit -am "commit auto des modifications des webservices KISS avec Swagger"
 git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev
