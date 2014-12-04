7814a560cb462d1b707ad18d719056fdd942104e
ibrahimacoundoul <Ibrahima.COUNDOUL@cpexterne.org>
2014-12-04 17:35:24 +0100

    ajout de HEAD sur le push

--- kiss-swagger-check-change.bat
+++ kiss-swagger-check-change.bat
@@ -1,5 +1,6 @@
 java -cp kissSwaggerDiff-0.0.1-SNAPSHOT.jar;D:\applis\MAVEN\m2repos\org\json\json\20140107\json-20140107.jar;D:\applis\GIT\kissSwaggerDiff\target\classes fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissSwaggerMainHandler
 REM on ajoute tous les nouveaux fichiers puis commiter et pusher sur la repos github 
+git pull
 git add *
 git commit -am "commit auto des modifications des webservices KISS avec Swagger"
-git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev
+git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev HEAD:master
