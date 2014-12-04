4f4baa1ee54355166ce8dd5ea3d0335371c3e848
ibrahimacoundoul <Ibrahima.COUNDOUL@cpexterne.org>
2014-12-04 15:06:45 +0100

    commit auto des fichiers json modifi├®s 4 d├®c bis

--- kiss-swagger-check-change.bat
+++ kiss-swagger-check-change.bat
@@ -1,2 +1,4 @@
 java -cp kissSwaggerDiff-0.0.1-SNAPSHOT.jar;D:\applis\MAVEN\m2repos\org\json\json\20140107\json-20140107.jar;D:\applis\GIT\kissSwaggerDiff\target\classes fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissSwaggerMainHandler
 REM git.exe commit -am "test comit auto"
+git commit -am "commit auto des fichiers json modifiés 4 déc bis"
+git push
