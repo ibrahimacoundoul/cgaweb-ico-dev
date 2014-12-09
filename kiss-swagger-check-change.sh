7b598ae03d152a867013fdc1521799f91b620fb3
ibrahimacoundoul <ibrahima.coundoul@cpxeterne.org>
2014-12-09 17:44:37 +0100

    ameliorations

--- kiss-swagger-check-change.sh
+++ kiss-swagger-check-change.sh
@@ -6,7 +6,8 @@
 #sudo chmod 777 -R src/main/resources/
 cd /var/lib/jenkins/jobs/ico-dev-build/workspace && java -cp target/kiss-swagger-monitoring-dep.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring
 #on ajoute tous les nouveaux fichiers puis commiter et pusher sur la repos github 
-git pull origin master
+#git pull origin master
+git branch --set-upstream-to=origin/master master
 git add *
 git commit -am "commit auto des modifications des webservices KISS avec Swagger 1"
 git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev
