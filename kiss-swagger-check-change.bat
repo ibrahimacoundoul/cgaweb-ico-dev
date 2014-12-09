201484d54180c8b32e504f839611a1ad9a088de3
ibrahimacoundoul <ibrahima.coundoul@cpxeterne.org>
2014-12-09 14:54:04 +0100

    commit auto des modifications des webservices KISS avec Swagger 1

--- kiss-swagger-check-change.bat
+++ kiss-swagger-check-change.bat
@@ -1,7 +1,7 @@
 REM java -cp kissSwaggerDiff-0.0.1-SNAPSHOT.jar;D:\applis\MAVEN\m2repos\org\json\json\20140107\json-20140107.jar;D:\applis\GIT\kissSwaggerDiff\target\classes;D:\applis\MAVEN\m2repos\org\codehaus\jackson\jackson-mapper-asl\1.9.13\jackson-mapper-asl-1.9.13.jar;D:\applis\MAVEN\m2repos\org\codehaus\jackson\jackson-core-asl\1.9.13\jackson-core-asl-1.9.13.jar   fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring
-java -cp target\uber.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring
+java -cp kiss-swagger-monitoring-dep.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring
 REM on ajoute tous les nouveaux fichiers puis commiter et pusher sur la repos github 
-REM git pull
-REM git add *
-REM git commit -am "commit auto des modifications des webservices KISS avec Swagger"
-REM git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev
+git pull
+git add *
+git commit -am "commit auto des modifications des webservices KISS avec Swagger 1"
+git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev
