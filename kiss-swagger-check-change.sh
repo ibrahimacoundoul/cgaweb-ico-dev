7a6a05c220c2cf8259c48cac6dd23f76b0ff374a
root <root@ip-172-23-1-188.eu-west-1.compute.internal>
2014-12-11 15:33:14 +0100

    commit auto des modifications des webservices KISS avec Swagger 1

--- kiss-swagger-check-change.sh
+++ kiss-swagger-check-change.sh
@@ -4,10 +4,21 @@
 
 
 # On va dans le repertoire oû il ya le jar de la classe et l'excéuter pour générer ou modifier les fichiers .json
-cd /var/lib/jenkins/jobs/ico-dev-build/workspace && java -cp target/kiss-swagger-monitoring-dep.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring
+#cd /var/lib/jenkins/jobs/ico-dev-build/workspace && java -cp target/kiss-swagger-monitoring-dep.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring
 #on ajoute tous les nouveaux fichiers puis commiter et pusher sur la repos github 
 #git pull origin master
-git branch --set-upstream-to=origin/master master
-git add *
-git commit -am "commit auto des modifications des webservices KISS avec Swagger 1"
-git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev
+#git branch --set-upstream-to=origin/master master
+#git add *
+#git -am "commit auto des modifications des webservices KISS avec Swagger 1"
+#git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev
+
+cd /var/lib/jenkins/jobs/ico-dev-build/workspace
+
+sudo git reset --hard HEADER # supprime tout (fichier non commit, etc)
+git checkout -f master # se positionne sur la branche et revert les fichiers, au cas où pull origin master # met àour le repo
+java -cp target/kiss-swagger-monitoring-dep.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring # generation du json
+
+sudo git add *
+sudo git commit -am "commit auto des modifications des webservices KISS avec Swagger 1"
+
+sudo git push --repo https://ibrahimacoundoul:testtest1@github.com/ibrahimacoundoul/cgaweb-ico-dev master
