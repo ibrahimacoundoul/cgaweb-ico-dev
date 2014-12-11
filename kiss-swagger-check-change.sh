aefd84c04c69c65c2b02e1fd2660b0f0478d3b06
ibrahimacoundoul <ibrahima.coundoul@cpxeterne.org>
2014-12-11 13:47:47 +0100

    ameliorations

--- kiss-swagger-check-change.sh
+++ kiss-swagger-check-change.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# java -cp kissSwaggerDiff-0.0.1-SNAPSHOT.jar;/d/applis/MAVEN/m2repos/org/json/json/20140107/json-20140107.jar;/d/applis/GIT/kissSwaggerDiff/target/classes fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissSwaggerMainHandler
-#cd /d/applis/GIT/kissSwaggerDiff
-#git commit -am "commit auto des fichiers json modifiés 4 déc bis"
-#git push
-#sudo chmod 777 -R src/main/resources/
+
+#sudo chmod 777 -R src/main/resources/*.json
+
+
+# On va dans le repertoire oû il ya le jar de la classe et l'excéuter pour générer ou modifier les fichiers .json
 cd /var/lib/jenkins/jobs/ico-dev-build/workspace && java -cp target/kiss-swagger-monitoring-dep.jar fr.canalplus.cgaweb.swagger.kissSwaggerDiff.KissWSApiDocsMonitoring
 #on ajoute tous les nouveaux fichiers puis commiter et pusher sur la repos github 
 #git pull origin master
